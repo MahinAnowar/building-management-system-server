b2602ca7276c4c457fff2b09e53bb57ac2fcdda6
Mahin Anowar <mahinanowar479@gmail.com>
2025-12-06 00:31:38 +0600

    cors middleware updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: ['https://building-management-system-client.vercel.app/'],
     credentials: true,
     optionsSuccessStatus: 200,
 };
