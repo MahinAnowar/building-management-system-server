cfebbcf8f1efd90e49021126acda3f5d5bc3b40e
Mahin Anowar <mahinanowar479@gmail.com>
2025-12-06 00:43:47 +0600

    cors error logging fix

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
-    origin: ['https://building-management-system-client.vercel.app/'],
+    origin: [
+        'http://localhost:5173',
+        'https://building-management-system-client.vercel.app'
+    ],
     credentials: true,
     optionsSuccessStatus: 200,
 };
@@ -242,6 +245,8 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Connected to BMS");
+    } catch (error) {
+        console.error("Database connection error:", error);
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close(); // Keep connection open for server
